8d44bbdfce0314ea995ce6cd53efec6d1d8a485c
Fernando <fernandobernardoo29@gmail.com>
2025-05-22 12:52:23 -0300

    implementação da area de texto

--- src/components/codeArea/index.tsx
+++ src/components/codeArea/index.tsx
@@ -1,9 +1,24 @@
 import styles from "./estilo.module.css";
 
-function CodeArea() {
+interface TextAreaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  className?: string;
+}
+
+function CodeArea({ className, ...rest }: TextAreaProps) {
   return (
     <div className={styles.container}>
-      <div className={styles.codeArea}>Code area</div>
+      <div className={styles.codeAreaContainer}>
+        <div className={styles.macFakeInterface}>
+          <div className={`${styles.circulo} ${styles.circuloVermelho}`} />
+          <div className={`${styles.circulo} ${styles.circuloAmarelo}`} />
+          <div className={`${styles.circulo} ${styles.circuloVerde}`} />
+        </div>
+        <textarea
+          className={`${styles.codeArea} ${className ?? ""}`}
+          {...rest}
+        />
+      </div>
     </div>
   );
 }
